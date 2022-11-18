7539bda861994460277cac1f753afe2e14f48e75
stefanmitovski <stef_mitovski6@hotmail.com>
2022-11-18 20:21:50 +0100

    novo

---
+++ JS1/02/JS/script.js
@@ -0,0 +1,11 @@
+// console.log("OK");
+// Objects
+var car = new Object();
+
+car.model="Fiat Lancia";
+car.godina=1994;
+
+console.log(car.model);
+console.log(car.godina);
+
+console.log(car['model'])
